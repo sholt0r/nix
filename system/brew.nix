d6be6ebfecd4b2763910e97cc376d461d64a9546
sholt0r <sholtor@kobu.au>
2024-10-10 07:09:53 +1100

    Added masApps to brew.

--- system/brew.nix
+++ system/brew.nix
@@ -15,8 +15,18 @@
 	    "onedrive"
       "secretive"
       "spotify"
+      "utm"
       "visual-studio-code"
     ];
+    masApps = {
+      "Bitwarden" = 1352778147;
+      "Microsoft Excel" = 462058435;
+      "Microsoft Outlook" = 985367838;
+      "Microsoft PowerPoint" = 462062816;
+      "Microsoft Word" = 462054704;
+      "Twingate" = 1501592214;
+      "Windows App" = 1295203466;
+    };
   	onActivation.cleanup = "zap";
   	onActivation.autoUpdate = true;
   	onActivation.upgrade = true;
