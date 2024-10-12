a66993ff6573d6f30441275bdade23e246cc045d
sholt0r <sholtor@kobu.au>
2024-10-12 14:41:03 +1100

    Changes to tmux config.

--- home/tmux.nix
+++ home/tmux.nix
@@ -4,37 +4,6 @@
     clock24 = true;
     keyMode = "vi";
     mouse = true;
-    sensibleOnTop = false;
-    extraConfig = ''
-      # Clock mode
-      setw -g clock-mode-colour "fg=green, bg=black"
-      
-      # Copy mode
-      setw -g mode-style "fg=cyan, bg=black, bold"
-      
-      # Panes
-      set -g pane-border-style "fg=black"
-      set -g pane-active-border-style "fg=cyan"
-      
-      # Statusbar
-      set -g status-position bottom
-      set -g status-style "fg=cyan bg=black"
-      
-      set -g status-left ""
-      
-      set -g status-right "%Y-%m-%d %H:%M "
-      
-      setw -g window-status-current-style "fg=black,bg=cyan"
-      setw -g window-status-current-format " #I #W #F "
-      
-      setw -g window-status-style "fg=cyan, bg=black"
-      setw -g window-status-format " #I #[fg=white]#W #[fg=yellow]#F impsons hit and run source code
-
-      
-      setw -g window-status-bell-style "fg=yellow, bg=green bold"
-      
-      # messages
-      set -g message-style "fg=green, bg=black, bold"
-    '';
+    shell = "/etc/profiles/per-user/jstaples/bin/zsh";
   };
 }
